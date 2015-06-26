80aba918e886fa18d9d9da27ae33e22053c393c4
omar-faruk-sust <omarfaruk.sust@gmail.com>
2015-06-26 01:42:20 -0400

    report show

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -109,11 +109,11 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth', 'namespace' => 'CMS']
     Route::group(['before' => 'perm:delete-return-files'], function () {
         Route::get('reports/{cms_report}/edit', ['as' => 'admin.reports.edit', 'uses' => 'ReportController@edit']);
         Route::put('reports/{cms_report}', ['as' => 'admin.reports.update', 'uses' => 'ReportController@update']);
-        Route::get('reports/{cms_report}/show', ['as' => 'admin.reports.show', 'uses' => 'ReportController@show']);
         Route::get('reports/{cms_report}/delete', ['as' => 'admin.reports.delete', 'uses' => 'ReportController@delete']);
     });
     
     Route::group(['before' => 'perm:download-payout-report'], function () {
+        Route::get('reports/{cms_report}/show', ['as' => 'admin.reports.show', 'uses' => 'ReportController@show']);
         Route::get('reports/{cms_report}/download', ['as' => 'admin.reports.download', 'uses' => 'ReportController@download']);
        });
 });
